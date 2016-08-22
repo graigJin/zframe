69f57d790994e776daf0e233c685382b8e9bf1f6
graigJin <zimmermann.email@web.de>
2016-08-22 15:16:42 +0200

    Adding UIwrapper class and initializing Display

--- zframe/src/zframe/display/Display.java
+++ zframe/src/zframe/display/Display.java
@@ -11,13 +11,18 @@ public class Display {
     private String title;
     private int width, height;
     private JFrame frame;
+    private UIwrapper wrapper;
 
     public Display(Handler handler, String title, int width, int height) {
         this.handler = handler;
         this.title = title;
         this.width = width;
         this.height = height;
+    }
+    
+    public void init() {
         createDisplay();
+        populateDisplay();
     }
     
     private void createDisplay() {
@@ -29,6 +34,12 @@ public class Display {
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+    
+    private void populateDisplay() {
+        wrapper = new UIwrapper(handler);
+        
+        frame.add(wrapper);
     }
 
 }
